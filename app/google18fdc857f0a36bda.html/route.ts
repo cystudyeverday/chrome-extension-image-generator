c03f9457d4e99d3389ced08e318c8b66a9298c5b
Chelsea <yang.cai@easyview.com.hk>
2026-06-30 17:58:17 +0800

    fix google issue

--- app/google18fdc857f0a36bda.html/route.ts
+++
@@ -1,9 +0,0 @@
-export const dynamic = "force-static";
-
-export function GET() {
-  return new Response("google-site-verification: google18fdc857f0a36bda.html", {
-    headers: {
-      "content-type": "text/html; charset=utf-8"
-    }
-  });
-}
